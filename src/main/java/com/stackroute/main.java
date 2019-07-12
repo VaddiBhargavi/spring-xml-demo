a2fbe2477ca61c2a7267ae3a4d09b52a72d37b52
VaddiBhargavi <bhargavivaddi999@gmail.com>
2019-07-12 10:18:23 +0530

    spring task6

--- src/main/java/com/stackroute/main.java
+++ src/main/java/com/stackroute/main.java
@@ -1,12 +1,13 @@
 package com.stackroute;
 
 import com.stackroute.demo.BeanLifecycleDemoBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class main {
     public static void main(String[] args) {
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+       AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         //BeanLifecycleDemoBean beanLifecycleDemoBean= (BeanLifecycleDemoBean) context.getBean("lifecycle");
        //beanLifecycleDemoBean.customInit();
        context.registerShutdownHook();
