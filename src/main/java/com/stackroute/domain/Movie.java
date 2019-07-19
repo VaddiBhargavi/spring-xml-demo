0e26c71086508fa2307ac9f87c40c00dbed80553
VaddiBhargavi <51950184+VaddiBhargavi@users.noreply.github.com>
2019-07-19 09:25:39 +0530

    Update Movie.java

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -4,14 +4,16 @@ public class Movie {
 
     //Movie "has-A" relation with Actor
     Actor actor;
-
+    //Default Constructor
     public Movie() {
     }
 
+    //Constructor
     public Movie(Actor actor) {
         this.actor = actor;
     }
 
+    //Display details of the actor
     public void movieDisplayInfo()
     {
         System.out.println("Actor class details");
