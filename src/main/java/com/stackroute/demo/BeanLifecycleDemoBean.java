2623f49e194a732568e89295543f9faa1355d54d
VaddiBhargavi <51950184+VaddiBhargavi@users.noreply.github.com>
2019-07-19 09:18:41 +0530

    Update BeanLifecycleDemoBean.java

--- src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
+++ src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
@@ -3,6 +3,8 @@ package com.stackroute.demo;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+//Implementing InitializingBean, DisposableBean
+
 public class BeanLifecycleDemoBean implements InitializingBean, DisposableBean {
     @Override
     public void destroy() throws Exception {
@@ -12,6 +14,7 @@ public class BeanLifecycleDemoBean implements InitializingBean, DisposableBean {
     public void afterPropertiesSet() throws Exception {
         System.out.println("Initialiser");
     }
+    //Custom methods
     public void customInit()
     {
         System.out.println("Initialise bean");
